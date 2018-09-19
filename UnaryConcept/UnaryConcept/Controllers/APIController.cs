c8354ec2727d1f608aef9eff1b252c457efa1cc0
RenMohan <38957639+RenMohan@users.noreply.github.com>
2018-09-19 13:33:35 -0400

    Add files via upload

---
+++ UnaryConcept/UnaryConcept/Controllers/APIController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnaryConcept.Core;
+using UnaryConcept.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UnaryConcept.Controllers
+{    
+    [Route("api/")]
+    public class APIController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public APIController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        // GET: api/<controller>
+        [HttpGet("parsing")]
+        public APIModel GetQuery(String searchQuery, String physicalPath, bool IncludeMachineSuggestions)
+        {
+            APIModel aPIModel = new APIModel();
+            String errorMsg = String.Empty;
+            GeneralFunctions generalFunctions = new GeneralFunctions();
+
+            if (string.IsNullOrWhiteSpace(searchQuery) && string.IsNullOrWhiteSpace(physicalPath))
+            {
+                string msg = "The Search Query and Physical path are required or missing";
+                aPIModel.ErrorMessage = msg;
+                generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, string.Empty, physicalPath, _environment);
+            }
+            else if (string.IsNullOrWhiteSpace(searchQuery))
+            {
+                string msg = "The Search Query is required or missing";
+                aPIModel.ErrorMessage = msg;
+                generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, string.Empty, physicalPath, _environment);
+            }
+            else if (string.IsNullOrWhiteSpace(physicalPath))
+            {
+                string msg = "The Physical path is required or missing";
+                aPIModel.ErrorMessage = msg;
+                generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, string.Empty, physicalPath, _environment);
+            }
+            else if (!generalFunctions.ValidateBalancedParentheses(searchQuery))
+            {
+                string msg = "Parenthesis is not balanced";
+                aPIModel.ErrorMessage = msg;
+                generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, string.Empty, physicalPath, _environment);
+            }
+            else if (!generalFunctions.ValidateBalancedCurlyBraces(searchQuery))
+            {
+                string msg = "Curly braces are not balanced";
+                aPIModel.ErrorMessage = msg;
+                generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, string.Empty, physicalPath, _environment);
+            }
+            else
+            {
+                string fileNameUploaded = string.Empty;
+                if (physicalPath.Contains(@"\"))
+                    fileNameUploaded = physicalPath.Substring(physicalPath.LastIndexOf('\\') + 1);
+                else
+                    fileNameUploaded = physicalPath;
+
+                String newFileNameWithSynonyms = string.Empty;
+
+                if (IncludeMachineSuggestions)
+                {
+                    //perform the validation here too for concept library 
+                    //create synonym words
+
+                    generalFunctions.ActivityLogMessageToFile("CreateSynonymWords (Method Begining)", "APIController", searchQuery, fileNameUploaded, physicalPath, _environment, string.Empty);
+
+                    Task<String> tempFileNameWithSynonyms = generalFunctions.CreateSynonymWords(null, fileNameUploaded, _environment, searchQuery);
+                    newFileNameWithSynonyms = tempFileNameWithSynonyms.Result;
+
+                    generalFunctions.ActivityLogMessageToFile("CreateSynonymWords (Method End)", "APIController", searchQuery, fileNameUploaded, physicalPath, _environment, "Output from CreateSynonymWords method : FileNameWithSynonyms = " + newFileNameWithSynonyms);
+                }
+
+                Task<Tuple<string, IFileInfo, string>> UploadFileResult;
+
+                if (!string.IsNullOrWhiteSpace(newFileNameWithSynonyms))
+                {
+                    generalFunctions.ActivityLogMessageToFile("UploadFile (Method Begining)", "APIController", searchQuery, newFileNameWithSynonyms, physicalPath, _environment, string.Empty);
+
+                    //Uploading .csv file into wwwroot path.
+                    UploadFileResult = generalFunctions.UploadFile(null, newFileNameWithSynonyms, _environment, true, searchQuery);
+
+                    generalFunctions.ActivityLogMessageToFile("UploadFile (Method End)", "APIController", searchQuery, newFileNameWithSynonyms, physicalPath, _environment, "Output from UploadFile method : FileName = " + UploadFileResult.Result.Item1 + "; FilePhysicalPath = " + UploadFileResult.Result.Item2.PhysicalPath);
+                }
+                else
+                {
+                    generalFunctions.ActivityLogMessageToFile("UploadFile (Method Begining)", "APIController", searchQuery, fileNameUploaded, physicalPath, _environment, string.Empty);
+
+                    //Uploading .csv file into wwwroot path.
+                    UploadFileResult = generalFunctions.UploadFile(null, fileNameUploaded, _environment, true, searchQuery);
+
+                    generalFunctions.ActivityLogMessageToFile("UploadFile (Method End)", "APIController", searchQuery, physicalPath, String.Empty, _environment, "Output from UploadFile method : FileName --" + UploadFileResult.Result.Item1 + "; FilePhysicalPath : " + UploadFileResult.Result.Item2.PhysicalPath);
+                    
+                }
+
+                if (!string.IsNullOrWhiteSpace(UploadFileResult.Result.Item3))
+                {
+                    aPIModel.ErrorMessage = UploadFileResult.Result.Item3;
+                }
+                else
+                {
+                    string fileName = UploadFileResult.Result.Item1;
+                    IFileInfo fileInfo = UploadFileResult.Result.Item2;
+
+                    if (String.IsNullOrEmpty(fileName))
+                    {
+                        string msg = "Invalid File Path";
+                        aPIModel.ErrorMessage = msg;
+                        generalFunctions.ErrorLogMessageToFile(msg, "GetQuery", "APIController", searchQuery, fileNameUploaded, physicalPath, _environment);
+                        return aPIModel;
+                    }
+
+                    QueryParserTemp qp = new QueryParserTemp();
+                    ConceptViewModel cvm = new ConceptViewModel();
+
+                    generalFunctions.ActivityLogMessageToFile("ParseQuery (Method Begining)", "APIController", searchQuery, physicalPath, string.Empty, _environment, string.Empty);
+
+                    //Parsing the Search query and creating Google and Bing queries.
+                    ConceptViewModel result = qp.ParseQuery(searchQuery, fileInfo.PhysicalPath, out aPIModel, out errorMsg, cvm, _environment);
+
+                    generalFunctions.ActivityLogMessageToFile("ParseQuery (Method End)", "APIController", searchQuery, physicalPath, string.Empty, _environment, "Output from ParseQuery method : UploadedFileName = " + result.UploadedFileName +
+                        "; UploadedFileNameFromDropDownList = " + result.UploadedFileNameAndDropDown + "; Translation = " + result.Translation + "; GoogleQuery = " + result.GoogleQuery + "; BingQuery = " + result.BingQuery);
+                    
+                    if (!string.IsNullOrWhiteSpace(errorMsg))
+                        aPIModel.ErrorMessage = errorMsg;
+                }
+            }
+            return aPIModel;
+        }
+    }
+}
---
+++ UnaryConcept/UnaryConcept/Controllers/HomeController.cs
@@ -0,0 +1,250 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnaryConcept.Model;
+using UnaryConcept.Core;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using System.Net.Http.Headers;
+using System;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace UnaryConcept.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+        private List<string> allCSVFilesList = new List<string>();
+        ConceptViewModel resultCvm = new ConceptViewModel();
+        string dropDownDefaultName = "Please select or upload a concept bank";
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IHostingEnvironment environment, ILogger<HomeController> logger)
+        {
+            _environment = environment;
+
+            string startFolder = _environment.WebRootPath;
+            DirectoryInfo dir = new DirectoryInfo(startFolder);                      
+            List<FileInfo> fileNamesList = dir.GetFiles("*.csv", SearchOption.AllDirectories).ToList();
+            resultCvm.UploadedFileNameAndDropDown = fileNamesList.Select(x => x.Name).ToList();
+            allCSVFilesList = resultCvm.UploadedFileNameAndDropDown;
+            allCSVFilesList.Insert(0, dropDownDefaultName);
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {               
+            return View(resultCvm);
+        }       
+
+        [HttpPost]
+        public IActionResult Index(ConceptViewModel vm, string fileNameUploaded)
+        {
+            string searchQueryForLog = string.Empty;
+            string uploadedFileNameForLog = string.Empty;
+            string fileNamePhysicalPathForLog = string.Empty;
+
+            GeneralFunctions generalFunctions = new GeneralFunctions();
+
+            _logger.LogInformation("Entered controller");
+            if (vm.resetClicked == "Reset")
+            {
+                ConceptViewModel resultCvm = new ConceptViewModel();
+                allCSVFilesList.Insert(0, dropDownDefaultName);
+                ModelState.Clear();
+            }
+            else
+            {
+                generalFunctions.ActivityLogMessageToFile("Index (Method Begining)", "HomeController", vm.SearchQuery, string.Empty, string.Empty, _environment, string.Empty);
+
+                if (!string.IsNullOrWhiteSpace(fileNameUploaded))
+                {
+                    fileNameUploaded = fileNameUploaded.Substring(fileNameUploaded.LastIndexOf('/') + 1);
+                    uploadedFileNameForLog = fileNameUploaded;
+                }
+
+                String errorMsg = String.Empty;
+
+                if (ModelState.IsValid)
+                {                    
+                    try
+                    {                        
+                        if (vm.LibPath == null && (vm.SearchQuery.Contains("{") || vm.SearchQuery.Contains("}")))
+                        {
+                            if (vm.LibPath == null && string.IsNullOrWhiteSpace(fileNameUploaded))
+                            {
+                                string msg = "*Please upload a valid Concept Bank";
+                                ModelState.AddModelError("SearchQuery", msg);
+                                generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                                return View(resultCvm);
+                            }
+                        }
+
+                        if (vm.SearchQuery != null)
+                        {
+                            searchQueryForLog = vm.SearchQuery;
+                            if (generalFunctions.ValidateBalancedParentheses(vm.SearchQuery))
+                            {
+                                if (!generalFunctions.ValidateBalancedCurlyBraces(vm.SearchQuery))
+                                {
+                                    string msg = "Curly braces are not balanced";
+                                    ModelState.AddModelError("SearchQuery", msg);
+                                    generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                                    return View(resultCvm);
+                                }
+                            }
+                            else
+                            {
+                                string msg = "Parenthesis is not balanced";
+                                ModelState.AddModelError("SearchQuery", msg);
+                                generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                                return View(resultCvm);
+                            }
+                        }
+
+                        if (vm.LibPath != null || (vm.LibPath == null && !string.IsNullOrWhiteSpace(fileNameUploaded)) || vm.LibPath == null)
+                        {
+                            String newFileNameWithSynonyms = string.Empty;
+
+                            if (vm.isSynonymChecked)
+                            {
+                                //perform the validation here too for concept library 
+                                //create synonym words
+                                generalFunctions.ActivityLogMessageToFile("CreateSynonymWords (Method Begining)", "HomeController", vm.SearchQuery, fileNameUploaded, string.Empty, _environment, string.Empty);
+
+                                Task<String> tempFileNameWithSynonyms = generalFunctions.CreateSynonymWords(vm.LibPath, fileNameUploaded, _environment, vm.SearchQuery);
+                                newFileNameWithSynonyms = tempFileNameWithSynonyms.Result;
+
+                                generalFunctions.ActivityLogMessageToFile("CreateSynonymWords (Method End)", "HomeController", vm.SearchQuery, fileNameUploaded, string.Empty, _environment, "Output from CreateSynonymWords method : FileNameWithSynonyms = " + newFileNameWithSynonyms);
+                            }
+
+                            Task<Tuple<string, IFileInfo, string>> UploadFileResult;
+
+                            if (!string.IsNullOrWhiteSpace(newFileNameWithSynonyms))
+                            {
+                                generalFunctions.ActivityLogMessageToFile("UploadFile (Method Begining)", "HomeController", vm.SearchQuery, newFileNameWithSynonyms, string.Empty, _environment, string.Empty);
+
+                                UploadFileResult = generalFunctions.UploadFile(null, newFileNameWithSynonyms, _environment, false, vm.SearchQuery);
+
+                                generalFunctions.ActivityLogMessageToFile("UploadFile (Method End)", "HomeController", vm.SearchQuery, newFileNameWithSynonyms, String.Empty, _environment, "Output from UploadFile method : FileName = " + UploadFileResult.Result.Item1 + "; FilePhysicalPath = " + UploadFileResult.Result.Item2.PhysicalPath);
+                            }
+                            else
+                            {
+                                generalFunctions.ActivityLogMessageToFile("UploadFile (Method Begining)", "HomeController", vm.SearchQuery, fileNameUploaded, fileNamePhysicalPathForLog, _environment, string.Empty);
+
+                                //Uploading .csv file into wwwroot path.
+                                UploadFileResult = generalFunctions.UploadFile(vm.LibPath, fileNameUploaded, _environment, false, vm.SearchQuery);
+
+                                generalFunctions.ActivityLogMessageToFile("UploadFile (Method End)", "HomeController", vm.SearchQuery, fileNameUploaded, String.Empty, _environment, "Output from UploadFile method : FileName --" + UploadFileResult.Result.Item1 + "; FilePhysicalPath : " + UploadFileResult.Result.Item2.PhysicalPath);
+                            }
+
+                            string fileName = UploadFileResult.Result.Item1;
+                            IFileInfo fileInfo = UploadFileResult.Result.Item2;
+                            errorMsg = UploadFileResult.Result.Item3;
+
+                            APIModel aPIModel = new APIModel();
+                            QueryParserTemp qp = new QueryParserTemp();
+
+                            if (fileInfo == null)
+                            {
+                                generalFunctions.ActivityLogMessageToFile("ParseQuery (Method Begining)", "HomeController", vm.SearchQuery, string.Empty, string.Empty, _environment, string.Empty);
+
+                                //Parsing the Search query and creating Google and Bing queries.
+                                resultCvm = qp.ParseQuery(vm.SearchQuery, null, out aPIModel, out errorMsg, resultCvm, _environment);
+
+                                generalFunctions.ActivityLogMessageToFile("ParseQuery (Method End)", "HomeController", vm.SearchQuery, string.Empty, string.Empty, _environment, "Output from ParseQuery method : UploadedFileName = " + resultCvm.UploadedFileName +
+                                    "; UploadedFileNameFromDropDownList = " + resultCvm.UploadedFileNameAndDropDown + "; Translation = " + resultCvm.Translation + "; GoogleQuery = " + resultCvm.GoogleQuery + "; BingQuery = " + resultCvm.BingQuery);
+                            }
+                            else
+                            {
+                                uploadedFileNameForLog = fileInfo.Name;
+                                fileNamePhysicalPathForLog = fileInfo.PhysicalPath;
+
+                                generalFunctions.ActivityLogMessageToFile("ParseQuery (Method Begining)", "HomeController", vm.SearchQuery, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment, string.Empty);
+
+                                //Parsing the Search query and creating Google and Bing queries.
+                                resultCvm = qp.ParseQuery(vm.SearchQuery, fileInfo.PhysicalPath, out aPIModel, out errorMsg, resultCvm, _environment);
+
+                                generalFunctions.ActivityLogMessageToFile("ParseQuery (Method End)", "HomeController", vm.SearchQuery, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment, "Output from ParseQuery method : UploadedFileName = " + resultCvm.UploadedFileName +
+                                    "; UploadedFileNameFromDropDownList = " + resultCvm.UploadedFileNameAndDropDown + "; Translation = " + resultCvm.Translation + "; GoogleQuery = " + resultCvm.GoogleQuery + "; BingQuery = " + resultCvm.BingQuery);
+                            }
+
+                            if ((String.IsNullOrEmpty(errorMsg) && vm.LibPath != null) || (String.IsNullOrEmpty(errorMsg) && vm.LibPath == null))
+                            {
+                                if (!string.IsNullOrWhiteSpace(fileName))
+                                {
+                                    int indx = allCSVFilesList.FindIndex(x => x == fileName);
+
+                                    if (indx >= 0)
+                                        allCSVFilesList.RemoveAt(indx);
+
+                                    allCSVFilesList.Insert(0, fileName);
+                                    resultCvm.UploadedFileNameAndDropDown = allCSVFilesList;
+
+                                    resultCvm.UploadedFileName = fileName;
+                                }
+                                else
+                                {
+                                    resultCvm.UploadedFileName = allCSVFilesList[0];
+                                    resultCvm.UploadedFileNameAndDropDown = allCSVFilesList;
+                                }
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("SearchQuery", errorMsg);
+                                generalFunctions.ErrorLogMessageToFile(errorMsg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {                       
+                        generalFunctions.ErrorLogMessageToFile(ex.Message, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);                        
+                    }
+                }
+                else
+                {
+                    foreach (var key in ModelState.Keys)
+                    {
+                        ModelState[key].Errors.Clear();
+
+                        if (vm.SearchQuery == null && vm.LibPath == null)
+                        {
+                            string msg = "*The Search Query and Concept Bank are required";
+                            ModelState.AddModelError("SearchQuery", msg);
+                            generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                        }
+                        else if (vm.SearchQuery == null)
+                        {
+                            string msg = "*The Search Query is required";
+                            ModelState.AddModelError("SearchQuery", msg);
+                            generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                        }
+                        else if (vm.LibPath == null)
+                        {
+                            string msg = "*Please upload a valid Concept Bank";
+                            ModelState.AddModelError("SearchQuery", msg);
+                            generalFunctions.ErrorLogMessageToFile(msg, "Index", "HomeController", searchQueryForLog, uploadedFileNameForLog, fileNamePhysicalPathForLog, _environment);
+                        }
+                    }
+                }
+            }
+            generalFunctions.ActivityLogMessageToFile("Index (Method End)", "HomeController", vm.SearchQuery, resultCvm.UploadedFileName, fileNamePhysicalPathForLog, _environment, "Return value from Index method : " + "UploadedFileNameFromDropDownList = " + resultCvm.UploadedFileNameAndDropDown + "; Translation = " + resultCvm.Translation + "; GoogleQuery = " + resultCvm.GoogleQuery + "; BingQuery = " + resultCvm.BingQuery);
+            return View(resultCvm);
+        }
+
+        public IActionResult Options()
+        {
+            return View();
+        }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+    }
+}
